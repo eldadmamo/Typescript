378844dab15b09b4f5588d4d4dfe6756c20a1976
Eldad <eldadf456@gmail.com>
2023-11-05 14:11:07 +0300

    updated

---
+++ annotations/metadata.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+
+// const plane = {
+//     color: 'red'
+// }
+//
+// Reflect.defineMetadata('note','Hi There', plane, 'color');
+// Reflect.defineMetadata('height', 10, plane);
+//
+//
+// const note = Reflect.getMetadata('note', plane, 'color');
+// const height = Reflect.getMetadata('height', plane);
+// console.log(note);
